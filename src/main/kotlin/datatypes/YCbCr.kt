18e327056e9fbf2ae157d676ea36c2c8786afda8
TheMetalStorm <arapoglu.simon@gmx.de>
2023-10-17 01:24:04 +0200

    YCbCr better print

--- src/main/kotlin/datatypes/YCbCr.kt
+++ src/main/kotlin/datatypes/YCbCr.kt
@@ -1,7 +1,12 @@
 package datatypes
 
+import java.math.RoundingMode
+import java.text.DecimalFormat
+
 data class YCbCr(var y: Double = 0.0, var cb: Double = 0.0, var cr: Double = 0.0){
     override fun toString(): String {
-        return "(y=$y, cb=$cb, cr=$cr) "
+        val df = DecimalFormat("0.000")
+        df.roundingMode = RoundingMode.CEILING
+        return "(y=${df.format(y)}, cb=${df.format(cb)}, cr=${df.format(cr)}) "
     }
 }
