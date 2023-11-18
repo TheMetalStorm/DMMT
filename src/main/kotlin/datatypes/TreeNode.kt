cb1f30db5f180531c7c22c3169a4a203c5bdffa4
Arapoglu, Simon <simon.arapoglu@student.fhws.de>
2023-11-18 16:20:20 +0100

    visualize tree

--- src/main/kotlin/datatypes/TreeNode.kt
+++ src/main/kotlin/datatypes/TreeNode.kt
@@ -1,5 +1,6 @@
 package datatypes
 
+
 data class TreeNode(val symbol: Int, val frequency: Int, var largestAmountOfStepsToLeaf: Int){
     var parent: TreeNode? = null
 
@@ -10,12 +11,31 @@ data class TreeNode(val symbol: Int, val frequency: Int, var largestAmountOfStep
         node.parent = this
     }
     override fun toString(): String {
-        return "TreeNode(symbol=$symbol, frequency=$frequency, depth=$largestAmountOfStepsToLeaf)"
+        val buffer = java.lang.StringBuilder(500)
+        print(buffer, "", "")
+        return buffer.toString()    }
+
+    private fun print(buffer: StringBuilder, prefix: String, childrenPrefix: String) {
+        buffer.append(prefix)
+        buffer.append(symbol)
+        buffer.append('\n')
+        val it: Iterator<TreeNode> = children.iterator()
+        while (it.hasNext()) {
+
+            val next = it.next()
+            if (it.hasNext()) {
+                next.print(buffer, "$childrenPrefix├0─ ", "$childrenPrefix│   ")
+            } else {
+                next.print(buffer, "$childrenPrefix└1─ ", "$childrenPrefix    ")
+            }
+        }
     }
     companion object {
         fun empty(): TreeNode{
             return TreeNode(Int.MIN_VALUE, Int.MAX_VALUE, 0)
         }
+
+
     }
 
 
