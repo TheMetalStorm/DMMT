86b5cb303369ae9794e460f15db8f8ed177d98f4
00Brot <richteralexanderjf@gmail.com>
2023-11-26 20:50:41 +0100

    add DHT to jpg class

--- src/main/kotlin/datatypes/JPG.kt
+++ src/main/kotlin/datatypes/JPG.kt
@@ -1,7 +1,8 @@
 package datatypes
 
 import JPGSegments.APP0
+import JPGSegments.DHT
 import JPGSegments.SOF0
 
-class JPG(apP0: APP0, soF0: SOF0) {//here will the other segments be added to create the jpeg at the end
+class JPG(apP0: APP0, soF0: SOF0, dht: List<DHT>) {//here will the other segments be added to create the jpeg at the end
 }
